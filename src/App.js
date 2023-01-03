0b8e084b430dee756237c046290a26082c9fc741
rsm917 <112781994+rsm917@users.noreply.github.com>
2023-01-03 15:47:50 -0600

    created a basic search function
    removed unnecessary stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer> this project was coded by Rebecca Shuping</footer>
+      </div>
     </div>
   );
 }
