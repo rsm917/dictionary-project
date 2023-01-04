fdafd1494f711f889f736776f7ff6d73e2e3f5ce
rsm917 <112781994+rsm917@users.noreply.github.com>
2023-01-04 17:32:23 -0600

    adjusted the number of definitions displayed

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,19 +9,23 @@ export default function Meaning(props) {
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              <strong>Definition: </strong>
-              {definition.definition}
-              <br />
+        if (index < 3) {
+          return (
+            <div key={index}>
+              <p>
+                <strong>Definition: </strong>
+                {definition.definition}
+                <br />
 
-              <Example example={definition.example} />
+                <Example example={definition.example} />
 
-              <br />
-            </p>
-          </div>
-        );
+                <br />
+              </p>
+            </div>
+          );
+        } else {
+          return <></>;
+        }
       })}
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
